36265646f2329397e188f89753592fe4f0d6330f
Kayode Alao <Oluwakayode.alao@gmail.com>
2019-09-22 22:57:23 +0100

    Add files via upload

---
+++ precache-manifest.925de57d41209d0e353f8f14e9a4b5da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a659664c85ea11f55e2244f5434250",
+    "url": "/index.html"
+  },
+  {
+    "revision": "882fb664f88382d4b2b1",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9a96d5d6f7e97222a8a0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "882fb664f88382d4b2b1",
+    "url": "/static/js/2.9211929e.chunk.js"
+  },
+  {
+    "revision": "9a96d5d6f7e97222a8a0",
+    "url": "/static/js/main.07330448.chunk.js"
+  },
+  {
+    "revision": "84f6fb56c13ad9da76d6",
+    "url": "/static/js/runtime-main.8580e661.js"
+  }
+]);
